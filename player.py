7c645c9e53153bb6fbbdd738a275ccaf0e57c78c
Matthew Walters <mwalte21@vols.utk.edu>
2021-09-17 16:51:29 -0400

    found some useful QT modules, and started some stuff for the player class #1

--- player.py
+++ player.py
@@ -1,3 +1,11 @@
 '''
     this is the file for all things related to the player object
 '''
+from PyQt6.QtWidgets import QGraphicsScene, QApplication, QGraphicsView, QGraphicsItem 
+class player(QGraphicsItem):
+    def __init__(self):
+        #not sure how exactly QGraphicsItems are initialized, so check on this
+        super().__init__()
+        #test different start positions for when it is added to the play area
+        self.setX(100)
+        self.setY(100)
