942f6bbaebaca0dcaec7bcd4daa697796033a334
Lillian Sharpe <lsharpe8@vols.utk.edu>
2021-09-27 15:56:04 -0400

    Completely restructured it so that a canvas (QLabel) with a QPixmap is not used, which fixes the resizing issue. Also made the buttons a fixed size and aligned them. Closes #3.

--- area.py
+++ area.py
@@ -1,72 +1,66 @@
 '''
     area.py
     teamlit
-    Creates an application window with a canvas
+    Creates a resizable application window with two buttons
 '''
 
 import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QColor, QPixmap
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton
+from PyQt6.QtGui import QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # Specify the dimensions and background color of the window
+        self.setGeometry(100, 100, 400, 300)
+        self.setStyleSheet("background-color: blue")
         
+
+        # Create a widget with a button layout at the top right of the window
         centralWidget = QWidget()
-
-        centralLayout = QVBoxLayout()
-
-        # PyQt5: centralLayout.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
-
-        centralWidget.setLayout(centralLayout)
-
 
         self.buttonLayout = QHBoxLayout()
 
+        # PyQt5: buttonLayout.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
+
+        self.buttonLayout.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
+
+
+        # Add a pause button to the button layout
         self.pauseButton = QPushButton()
         self.pauseButton.setText("Pause")
+        self.pauseButton.setStyleSheet("background-color: lightGray;"
+                                        "border-style: outset;"
+                                        "border-width: 1px;"
+                                        "border-color: black;"
+                                        "min-width: 40 em;"
+                                        "max-width: 40 em;"
+                                        "padding: 6 px;")
+                                        
         self.buttonLayout.addWidget(self.pauseButton)
         
+        # Add an exit button to the button layout
         self.exitButton = QPushButton()
         self.exitButton.setText("Exit")
+        self.exitButton.setStyleSheet("background-color: lightGray;"
+                                        "border-style: outset;"
+                                        "border-width: 1px;"
+                                        "border-color: black;"
+                                        "min-width: 40 em;"
+                                        "max-width: 40 em;"
+                                        "padding: 6 px;")
         self.exitButton.clicked.connect(self.exitClicked)
         self.buttonLayout.addWidget(self.exitButton)
 
-        centralLayout.addLayout(self.buttonLayout)
-
-
-        self.canvas = Canvas()
-        centralLayout.addWidget(self.canvas)
-
+        # Add the button layout to the widget and set the widget as the central widget
+        centralWidget.setLayout(self.buttonLayout)
         self.setCentralWidget(centralWidget)
+
         
-
-    # Resize can adjust to expanding the window but the window cannot be sized down
-    def resizeEvent(self, event):
-        self.canvas.resize(self.width(), self.height())
-
-
     def exitClicked(self, event):
         sys.exit(app)
-
-
-class Canvas(QLabel):
-    def __init__(self):
-        super().__init__()
-
-        #PyQt5: self.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-
-        # Initialize pixmap with a size and color
-        self.pixmap = QPixmap(1000, 500)
-        self.pixmap.fill(QColor("blue"))
-        self.setPixmap(self.pixmap)
-
-
-    def resizeEvent(self, event):
-        self.pixmap = self.pixmap.scaled(self.width(), self.height())
-        self.setPixmap(self.pixmap)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
