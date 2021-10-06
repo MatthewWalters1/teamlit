c991d731afe0d8824c74a3a75f0b5aaa7144fa7b
Victor Hanset <vhanset@vols.utk.edu>
2021-10-06 19:03:56 -0400

    minor changes

--- player.py
+++ player.py
@@ -13,14 +13,14 @@ class player(QGraphicsItem):
         #test different start positions for when it is added to the play area
         self.setX(100)
         self.setY(100)
-        self.xVel = 0
-        self.yVel = 0
         #this is the bounding rectangle of the player object in the graphics scene, and the image itself
         self.boundingRect(0, 0, 30, 30)
         self.ship = QPixmap()
         self.ship.load("fighter.png")
     
     def keyPressEvent(self, event):
+        xVel = 0
+        yVel = 0
         if event.key() == Qt.Key_Left:
             #change velocitiy
             xVel = -20 #may change if too fast/slow
@@ -37,10 +37,4 @@ class player(QGraphicsItem):
             #change velocity
             yVel = 20 #may change if too fast/slow
 
-
-##idea## score class
-class score(QGraphicsItem):
-    def __init__(self):
-        super.__init__(self)
-    
-        self.score = 0
+        self.setPos(self.x()+xVel, self.y()+yVel)
