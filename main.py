9bdd614737bd377b1f1300a690541fb472773a6b
jacobgpagee <67118047+jacobgpagee@users.noreply.github.com>
2021-10-07 01:03:58 -0400

    Cleaned up main.py and added in end game if the timer reaches a certain limit part of #8

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import sys
-import area
-from PyQt6.QtWidgets import QGraphicsView, QWidget,QPushButton,QApplication,QListWidget,QGridLayout,QLabel
-from PyQt6.QtCore import QTimer,QDateTime
-from PyQt6.QtCore import Qt
+'''
+
+    Project Name: Teamlit CS340 Project
+    Authors: Jacob Page, Matthew Walters, Lillian Sharpe, and Victor Hanset
+    This is the main for the project and contains the timer class and brings the play field, menus, and the player togther to form the game
+
+'''
+from PyQt6.QtWidgets import QGraphicsView, QWidget, QLabel, QApplication, QVBoxLayout
+from PyQt6.QtCore import QTimer, Qt
 from PyQt6.QtGui import QBrush, QColor
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QGraphicsScene, QGraphicsItem, QMessageBox
+import sys, area
 
 class Timer(QWidget): #Manages the game timer
     def __init__(self):
         super().__init__()
 
-        self.displayTime=QLabel('0') #Creates the label that the time will be printed on
-
-        layout=QVBoxLayout()
-        layout.setAlignment(Qt.AlignmentFlag.AlignBottom)
-
-        self.timer=QTimer()
-
-        self.timer.timeout.connect(self.showTime)
-        self.timeLeft = 0
-
+        self.displayTime = QLabel('0') #Creates the label that the time will be printed on
+        self.timer = QTimer()
+        layout = QVBoxLayout()
+        
+        layout.setAlignment(Qt.AlignmentFlag.AlignBottom) #Layout to display the timer
         layout.addWidget(self.displayTime)
         self.setLayout(layout)
 
+        self.timer.timeout.connect(self.showTime) #Intializes the timer to 0 and starts the timer
+        self.timeLeft = 0
         self.startTimer()
 
     def showTime(self):
@@ -34,28 +35,26 @@ class Timer(QWidget): #Manages the game timer
         self.timer.start(1000)
         self.timer2.start(10)
 
-    def endTimer(self):
+    def pauseTimer(self): #Pauses the timer
         self.timer.stop()
 
+    def endGame(self): #This denotes the game has ended and all options will now be displayed
+        self.pauseTimer()
+        sys.exit()
 
 if __name__ == '__main__':
-    app=QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
     window = area.Window()
-    
-    form=Timer()
-    window.buttonLayout.addWidget(form)
-
-    window.pauseButton.clicked.connect(form.endTimer)
-    window.resumeButton.clicked.connect(form.startTimer)
-
-    #window.setBackgroundBrush(QBrush(QColor(200, 0, 0)))
-
-    form.show()
+    form = Timer()
     view = QGraphicsView(window)
 
-    #view.setBackgroundBrush(QBrush(QColor(200, 0, 0)))
+    window.buttonLayout.addWidget(form)
+    window.pauseButton.clicked.connect(form.pauseTimer)
+    window.resumeButton.clicked.connect(form.startTimer)
+    window.setBackgroundBrush(QBrush(QColor(173, 216, 230)))
 
     view.show()
+    form.show()
 
     sys.exit(app.exec())
